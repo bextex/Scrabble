e76b77ae922600c2182e2d871c9db733874208f7
snabelA00 <ja000411@gmail.com>
2020-11-14 15:39:41 +0100

    jquery file and nav.js added

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+$('nav .rulesBtn').click(function () {
+  $('.rulesContainer').css(
+    "display", "block"
+  );
+  console.log("clicked close");
+});
+
+$('.rulesContainer .closeRulesBtn').click(function () {
+  $('.rulesContainer').css(
+    "display", "none"
+  );
+  console.log("clicked close");
+});
