ab920e1dfbc45d6a4a8a7405c9fe2fbe10db9a6e
bextex <r.sarvegard@gmail.com>
2020-11-16 15:10:57 +0100

    Added players names

--- js/player.js
+++ js/player.js
@@ -23,4 +23,29 @@ export default class Player {
     });
   }
 
+
+  setPlayerNames(inputName) {
+    // if (countClicks == 1) {
+    //   console.log(document.getElementById(`player1Name`).value);
+    //   if (document.getElementById(`player1Name`).value === null) {
+    //   }
+    // }
+
+    players.push(new Player(inputName));
+    console.log(players);
+
+
+    // let index = 1;
+    //   $(".playersName").children().each(function () {
+    //     let name;
+    //     if (document.getElementById(`player${index}Name`).value === null) {
+    //       name = 'Player ' + index;
+    //     } else {
+    //       name = document.getElementById(`player${index}Name`).value;
+    //     }
+    //     index++;
+    //   });
+    // }
+  }
+
 }
