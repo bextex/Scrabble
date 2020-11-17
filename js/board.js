4267e402cf679de74b4f017e66dec2b69eb00e87
bextex <r.sarvegard@gmail.com>
2020-11-17 19:08:28 +0100

    Added special characters to board

--- js/board.js
+++ js/board.js
@@ -7,15 +7,60 @@ export default class Board {
   }
 
   createBoard() {
-    this.board = [...new Array(15)].map(x => new Array(15).fill({
-      specialValue: '2w', tile: undefined
-    }));
+    // this.board = [...new Array(15)].map(x => new Array(15).fill({
+    //   specialValue: '2w', tile: undefined
+    // }));
+
+    this.board = [...new Array(15)].map(x => [...new Array(15)].map(x => ({})));
+
+
+    // Add some info about special squares
+    // Triple word score (3xWs) or swedish (3xOp) Op = Ordpoäng
+    [[0, 0], [0, 7], [0, 14], [7, 0], [7, 14], [14, 0], [14, 7], [14, 14]]
+      .forEach(([y, x]) => this.board[y][x].special = '3xWS');
+    // Double letter score (2xLs) or swedish (2xBp) Bp = bokstavspoäng
+    [[0, 3], [0, 11], [2, 6], [2, 8], [3, 0], [3, 7], [3, 14], [6, 2], [6, 6], [6, 8], [6, 12], [7, 3], [7, 11],
+    [8, 2], [8, 6], [8, 8], [8, 12], [11, 0], [11, 7], [11, 14], [12, 6], [12, 8], [14, 3], [14, 11]]
+      .forEach(([y, x]) => this.board[y][x].special = '2xLS');
+    // Triple letter score (3xLs) or swedish (3xOp)
+    [[1, 5], [1, 9], [5, 1], [5, 5], [5, 9], [5, 13], [9, 1], [9, 5], [9, 9], [9, 13], [13, 5], [13, 9]]
+      .forEach(([y, x]) => this.board[y][x].special = '3xLS');
+    // Double word score (2xWs) or swedish (3xBp)
+    [[1, 1], [1, 13], [2, 2], [2, 12], [3, 3], [3, 11], [4, 4], [4, 10], [7, 7], [10, 4], [10, 10], [11, 3], [11, 11],
+    [12, 2], [12, 12], [13, 1], [13, 13]]
+      .forEach(([y, x]) => this.board[y][x].special = '2xWS');
+    this.board[7][7].special = 'middle-star';
+
   }
 
   render() {
-    $('.board').remove();
-    let $board = $('<div class="board"/>').appendTo('.playing-window');
-    this.board.flat().forEach(x => $board.append('<div/>'));
+    // $('.board').remove();
+    // let $board = $('<div class="board"/>').appendTo('.playing-window');
+    // this.board.flat().forEach(x => $board.append('<div/>'));
+
+    //********************************************* */
+
+    if (!$('.board').length) {
+      $('.playing-window').append(`
+        <div class="board"></div>
+        <div class="tiles"></div>
+      `);
+    }
+
+    // render the board
+    console.log(this.board.flat());
+    $('.board').html(
+      this.board.flat().map(x => `
+        <div class="${x.special ? 'special-' + x.special : ''}">
+          ${x.tile ? `<div class="tile">${x.tile.char}</div>` : ''}
+        </div>
+      `).join('')
+    );
+
+    // render the tiles
+    // $('.tiles').html(
+    //   this.tiles.map(x => `<div>${x.char}</div>`).join('')
+    // );
 
   }
 
@@ -35,30 +80,3 @@ $('.rulesContainer .closeRulesBtn').click(function () {
   );
 });
 
-function createBoard() {
-  // Create a 10 x 10 board with an empty object
-  // for each square
-
-  // Creating the board
-  //this.board = [...new Array(15)]
-  //  .map(x => [...new Array(15)].map(x => ({})));
-
-
-  // Add some info about special squares
-  // Triple word score (3xWs) or swedish (3xOp) Op = Ordpoäng
-  [[0, 0], [0, 7], [0, 14], [7, 0], [7, 14], [14, 0], [14, 7], [14, 14]]
-    .forEach(([y, x]) => this.board[y][x].special = 'red').addClass('3xWS');
-  // Double letter score (2xLs) or swedish (2xBp) Bp = bokstavspoäng
-  [[0, 3], [0, 11], [2, 6], [2, 8], [3, 0], [3, 7], [3, 14], [6, 2], [6, 6], [6, 8], [6, 12], [7, 3], [7, 11],
-  [8, 2], [8, 6], [8, 8], [8, 12], [11, 0], [11, 7], [11, 14], [12, 6], [12, 8], [14, 3], [14, 11]]
-    .forEach(([y, x]) => this.board[y][x].special = 'light blue').addClass('2xLS');
-  // Triple letter score (3xLs) or swedish (3xOp)
-  [[1, 5], [1, 9], [5, 1], [5, 5], [5, 9], [5, 13], [9, 1], [9, 5], [9, 9], [9, 13], [13, 5], [13, 9]]
-    .forEach(([y, x]) => this.board[y][x].special = 'dark blue').addClass('3xLS');
-  // Double word score (2xWs) or swedish (3xBp)
-  [[1, 1], [1, 13], [2, 2], [2, 12], [3, 3], [3, 11], [4, 4], [4, 10], [7, 7], [10, 4], [10, 10], [11, 3], [11, 11],
-  [12, 2], [12, 12], [13, 1], [13, 13]]
-    .forEach(([y, x]) => this.board[y][x].special = 'pink').addClass('2xWS');
-  [7, 7].addClass('middle-star');
-
-}
