ebcd970d81fda43786614524995b3fda687ad978
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-11-26 09:55:55 +0100

    Commented out SCRABBLE

--- Start.js
+++ Start.js
@@ -51,8 +51,9 @@ $('.start-game').on('click', function () {
   setTimeout(() => {
     new Board().start(that.tiles);
   }, 1700);
-  $('.scrabble').animate({ top: '12px' }, 'slow');
-  $('.scrabble').animate({ fontSize: '40px' }, 'slow');
+  $('.scrabble').fadeOut(2000);
+  // $('.scrabble').animate({ top: '12px' }, 'slow');
+  //$('.scrabble').animate({ fontSize: '40px' }, 'slow');
   // console.log("new Game().playerTurn - called")
   //new Game(that.tiles);
   // new Game().countScore();
