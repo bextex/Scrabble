9e3ea1ac70317d5bea6ca0d30c6bba846834564a
bextex <r.sarvegard@gmail.com>
2020-12-17 20:47:32 +0100

    Revert "cleaned up code in board, network, player and start"
    This reverts commit 5cdbe10922ed10a0aadf9fe5eafcf31054c020a7.

--- js/board.js
+++ js/board.js
@@ -1,8 +1,15 @@
 import Game from './game.js';
 
+
 export default class Board {
 
+  // Doesn't really need board anymore, easier to have all in board 
+
   async start(tilesFromBag) {
+
     new Game(tilesFromBag);
+
+    // console.log(this.board);
+
   }
 }
--- js/network.js
+++ js/network.js
@@ -1,31 +1,46 @@
 import Store from 'https://network-lite.nodehill.com/store';
+import Start from '../Start.js';
 import Game from './game.js';
+import Player from './player.js';
 import Bag from './bag.js';
+import Modal from './modal.js';
+
 
 export let store;
+// export let playerIndex_new;
 
 export default class Network {
 
   constructor() {
+
     localStorage.clear();
     this.localStore = Store.getLocalStore();
+    // this.tilesFromBag = tilesFromBag;
+
+
+
+    // this.start();
   }
+
 
   async getLocalKey() {
     this.localStore.networkKey = await Store.createNetworkKey();
     return this.localStore.networkKey;
   }
 
+  // setLocalKey() {
+  //   return this.connectToChat();
+  // }
+
   async connectToStore(networkKey, playerName) {
+
     // Put name in this localStore.name and then make it more accessable with local name
     this.localStore.name = playerName;
     let name = this.localStore.name;
 
-    // Get the tiles from bag
     let bag = new Bag();
     this.tilesFromFile = await bag.tilesFromFile();
 
-    // Create a new game
     let game = new Game();
 
     // this.networkKey is either a created key or a inserted key
@@ -38,51 +53,72 @@ export default class Network {
     let s = this.networkStore;
     store = s;
 
-    // If players pass or change tiles 3x in a row - the game end
     s.passcounter = 0;
     s.playercount = 0;
 
-    // The name of the players
+    // We want the network to listen for which players connecting to the same game (same game key)
     s.players = s.players || [];
-    // The score of the players with name
     s.score = s.score || [];
-    // The score from the current tiles the player have on rack
     s.scoreFromTileLeftOnRack = s.scoreFromTileLeftOnRack || [];
-    // The potential score if counting with the rules of rack-tiles after game end
     s.potentialTotalScore = s.potentialTotalScore || [];
-    // The words that have been played on board
+
     s.storeOldWords = s.storeOldWords || [];
-    // The currentWords playing on board
     s.storeCurrentWords = s.storeCurrentWords || 0;
-    // Currentplayer when game starts
+
     s.currentPlayer = 0;
-    // The tiles from file save as a listener variable to be shared between players
+
     s.tilesFromFile = s.tilesFromFile || this.tilesFromFile;
-    // The playing board
+
     s.board = s.board || game.createBoard();
+    console.log(s.board);
 
     // We want to listen for which player is the one currently playing
     // s.currentPlayer = s.currentPlayer || game.playerIndex;
     console.log('Current player has index:', s.currentPlayer);
     console.log('The playercount is', s.playercount)
 
+    // We want to if we're not in the same game anymore
+    // s.game = s.game || game;
+    // console.log(s.game);
+
+
     // Which player index am I? (0, 1, 2 or 3?)
     this.playerIndexInNetwork = s.players.length;
+    console.log('My index is ' + this.playerIndexInNetwork);
+
+
 
     // Add my name to s.players array
     s.players.push(name);
-    // Add name and score to score
+    console.log('my name is ' + name);
+    console.log(s.players);
+
     s.score.push({ name: name, points: 0 });
 
+    // For all players except the one starting the game will need a render of the board
+
+    // game.start();
     console.log('The current player is ' + s.players[s.currentPlayer]);
 
-    // For all players except the one that starts
+    // This should only be at the beginning when joining a game
+    // if (s.players.length > 1) {
+    //   this.game.start();
+    //   $('.playing-window-left').append(`<div class="not-your-turn">Vänta på att spelet ska starta</div>`);
+    // }
+
+
+    // s.game = s.game || game;
+
+
     if (s.players.length > 1) {
       game.start(name, this.playerIndexInNetwork);
     }
 
     // The player that gets a game-key is the only player that can start the game,
     // because they are the only one with the start-button
+
+
+
     let that = this;
 
     $('.start-new-game').on('click', async function () {
@@ -106,19 +142,26 @@ export default class Network {
       //   // alert('This is a modal');
       // }
     });
+
+    console.log('----------------------we are less than 2')
   }
 
 
 
   listenForNetworkChanges(game) {
     let s = this.networkStore;
-    console.log('This count as a network change');
+    // let name = this.localStore.name;
 
-    // Append a waiting box for the game starter to know who and how many players are joining
+    console.log('This count as a network change');
+    // s.currentPlayer = this.playerIndexInNetwork;
+    // let currentPlayerName = s.players[s.currentPlayer];
+
+
     if ($('.waiting-for-players').length) {
       $('.waiting-box').empty();
       for (let i = 1; i < s.players.length; i++) {
         console.log('there are currently ' + s.players.length + ' players in player array');
+
         $('.waiting-box').append(`
         <br>
           ${s.players[i]} har joinat spelet</br>
@@ -126,14 +169,37 @@ export default class Network {
         s.playercount++;
       }
     }
+
+    // let game = s.game;
+
+    console.log('Current players index:', s.currentPlayer);
     console.log('Its ' + s.players[s.currentPlayer] + 's turn');
+    console.log('My playerindex in store:', this.playerIndexInNetwork);
+    console.log('Should I render?', (s.currentPlayer === this.playerIndexInNetwork))
     if (!$('.waiting-box').length) {
       game.board = s.board;
       game.tilesFromBag = s.tilesFromFile;
       game.storeOldWords = s.storeOldWords;
       game.storeCurrentWords = s.storeCurrentWords;
+      // game.playerTurn();
+
       game.render();
+      //Testing
+      // if (s.passcounter === 3) {
+      //   game.endgame();
+      // }
+      // console.log('From network, what is s.passcounter', s.passcounter);
     }
+
+    // } else if (!$('.waiting-box').length) {
+    //   $('.playing-window').empty();
+    //   if (store.players.indexOf(this.name) === store.currentPlayer) {
+    //     $('.not-your-turn').remove();
+    //   } else {
+    //     //this.render();
+    //     $('.playing-window').append(`<div class="not-your-turn"><p>${store.players[store.currentPlayer]} spelar just nu...</p></div>`);
+    //   }
+    // }
   }
 }
 
